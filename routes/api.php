dcdbcfa32066f9254e1698d26433efbc97c145f1
Cherny_Oleg <leo37rus@yandex.ru>
2025-01-31 12:37:56 +0300

    add roles

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ControllerExample;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    // список всех сообщений
+    Route::get('posts', [ControllerExample::class, 'post']);
+    // получить сообщение
+    Route::get('posts/{id}', [ControllerExample::class, 'singlePost']);
+    // добавить сообщение
+    Route::post('posts', [ControllerExample::class, 'createPost']);
+    // обновить сообщение
+    Route::put('posts/{id}', [ControllerExample::class, 'updatePost']);
+    // удалить сообщение
+    Route::delete('posts/{id}', [ControllerExample::class, 'deletePost']);
+    // добавить нового пользователя с ролью Writer
+    Route::post('users/writer', [ControllerExample::class, 'createWriter']);
+    // добавить нового пользователя с Subscriber
+    Route::post(
+        'users/subscriber',
+        [ControllerExample::class, 'createSubscriber']
+    );
+    // удалить пользователя
+    Route::delete('users/{id}', [ControllerExample::class, 'deleteUser']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('clear/token', function () {
+    if(Auth::check() && Auth::user()->role === 1) {
+        Auth::user()->tokens()->delete();
+    }
+    return 'Token Cleared';
+})->middleware('auth');
+
+Auth::routes();
+
+Route::post('register', [RegisterController::class, 'register'])
+    ->middleware('restrictothers');
+// Страница создания токена
+Route::get('dashboard', function () {
+    if(Auth::check() && Auth::user->role === 1){
+        return auth()
+            ->user()
+            ->createToken('auth_token', ['admin'])
+            ->plainTextToken;
+    }
+    return redirect("/");
+})->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
